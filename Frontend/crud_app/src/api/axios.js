7c40b398c3f9e2b83bd64e105d3d252e5921a014
Sahiru <sahiru906@gmail.com>
2025-06-27 23:55:23 +0530

    updated

--- Frontend/crud_app/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'http://localhost:5000/api', // Update to match your backend
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
